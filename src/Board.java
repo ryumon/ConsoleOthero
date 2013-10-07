5bb2bd0d481e20b8609f1d241b46da78646eab89
Ryumon <hasten.zx.zx.rr09@gmail.com>
2013-10-08 08:58:24 +0900

    Boardクラスを少し修正

--- src/Board.java
+++ src/Board.java
@@ -93,92 +93,92 @@ public class Board {
 		//四隅の判定
 		if(assign==0) {
 			if(masu[1] == a) {
-				checkAndFlipLine5(player1,x,y);
-				direct = 5;		
+				direct = 5;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 
 			if(masu[9] == a) {
-				checkAndFlipLine6(player1,x,y);
 				direct = 6;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[8] == a) {
-				checkAndFlipLine7(player1,x,y);
 				direct = 7;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			return direct;
 		}
 
 		if(assign==7) {
 			if(masu[6] == a) {
-				checkAndFlipLine1(player1,x,y);
 				direct = 1;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 
 			if(masu[15] == a) {
-				checkAndFlipLine7(player1,x,y);
 				direct = 7;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 
 			if(masu[14] == a) {
-				checkAndFlipLine8(player1,x,y);
 				direct = 8;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			return direct;
 		}
 
 		if(assign==56) {
 			if(masu[48] == a) {
-				checkAndFlipLine3(player1,x,y);
 				direct = 3;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[49] == a) {
-				checkAndFlipLine4(player1,x,y);
 				direct = 4;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 
 			if(masu[57] == a) {
-				checkAndFlipLine5(player1,x,y);
 				direct = 5;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			return direct;
 		}
 
 		if(assign==63) {
 			if(masu[62] == a) {
-				checkAndFlipLine1(player1,x,y);
 				direct = 1;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[54] == a) {
-				checkAndFlipLine2(player1,x,y);
 				direct = 2;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[55] == a) {
-				checkAndFlipLine3(player1,x,y);
 				direct = 3;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			return direct;
 		}
 		//上辺のチェック(端２つは除外)masu[1]~masu[6]について←↙↓↘→のマスを確認
 		if(1<=assign && assign<=6) {
 			if(masu[assign-1] == a) {
-				checkAndFlipLine1(player1,x,y);
 				direct = 1;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign+1] == a) {
-				checkAndFlipLine5(player1,x,y);
 				direct = 5;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign+9] == a) {
-				checkAndFlipLine6(player1,x,y);
 				direct = 6;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign+8] == a) {
-				checkAndFlipLine7(player1,x,y);
 				direct = 7;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign+7] == a) {
-				checkAndFlipLine8(player1,x,y);
 				direct = 8;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 
 			return direct;
@@ -186,24 +186,24 @@ public class Board {
 		//下辺のチェック(端２つは除外)masu[57]~masu[62]について←↖↑↗→のマスを確認
 		if(57<=assign && assign<=62) {
 			if(masu[assign-1] == a) {
-				checkAndFlipLine1(player1,x,y);
 				direct = 1;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign-9] == a) {
-				checkAndFlipLine2(player1,x,y);
 				direct = 2;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign-8] == a) {
-				checkAndFlipLine3(player1,x,y);
 				direct = 3;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign-7] == a) {
-				checkAndFlipLine4(player1,x,y);
 				direct = 4;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign+1] == a) {
-				checkAndFlipLine5(player1,x,y);
 				direct = 5;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 
 			return direct;
@@ -213,24 +213,24 @@ public class Board {
 		//左辺
 		if(assign % 8 == 0) {
 			if(masu[assign-8] == a) {
-				checkAndFlipLine3(player1,x,y);
 				direct = 3;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign-7] == a) {
-				checkAndFlipLine4(player1,x,y);
 				direct = 4;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign+1] == a) {
-				checkAndFlipLine5(player1,x,y);
 				direct = 5;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign+9] == a) {
-				checkAndFlipLine6(player1,x,y);
 				direct = 6;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign+8] == a) {
-				checkAndFlipLine8(player1,x,y);
 				direct = 7;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 
 			return direct;
@@ -238,24 +238,24 @@ public class Board {
 		//右辺
 		if((assign + 1) % 8 == 0) {
 			if(masu[assign-1] == a) {
-				checkAndFlipLine1(player1,x,y);
 				direct = 1;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign-9] == a) {
-				checkAndFlipLine2(player1,x,y);
 				direct = 2;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign-8] == a) {
-				checkAndFlipLine3(player1,x,y);
 				direct = 3;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign+8] == a) {
-				checkAndFlipLine7(player1,x,y);
 				direct = 7;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 			if(masu[assign+7] == a) {
-				checkAndFlipLine8(player1,x,y);
 				direct = 8;
+				checkAndFlipLine(player1,x,y,direct);
 			}
 
 			return direct;
@@ -263,54 +263,67 @@ public class Board {
 
 		//指定されたマスの左隣のマスから時計回りに
 		if(masu[assign-1] == a) {
-			checkAndFlipLine1(player1,x,y);
 			direct = 1;
-
+			checkAndFlipLine(player1,x,y,direct);
 		}
 		if(masu[assign-9] == a) {
-			checkAndFlipLine2(player1,x,y);
 			direct = 2;
-
+			checkAndFlipLine(player1,x,y,direct);
 		}
 		if(masu[assign-8] == a) {
-			checkAndFlipLine3(player1,x,y);
 			direct = 3;
+			checkAndFlipLine(player1,x,y,direct);
 
 		}
 		if(masu[assign-7] == a) {
-			checkAndFlipLine4(player1,x,y);
 			direct = 4;
-
+			checkAndFlipLine(player1,x,y,direct);
 		}
 		if(masu[assign+1] == a) {
-			checkAndFlipLine5(player1,x,y);
 			direct = 5;
-
+			checkAndFlipLine(player1,x,y,direct);
 		}
 		if(masu[assign+9] == a) {
-			checkAndFlipLine6(player1,x,y);
 			direct = 6;
-
+			checkAndFlipLine(player1,x,y,direct);
 		}
 		if(masu[assign+8] == a) {
-			checkAndFlipLine7(player1,x,y);
 			direct = 7;
-
+			checkAndFlipLine(player1,x,y,direct);
 		}
 		if(masu[assign+7] == a) {
-			checkAndFlipLine8(player1,x,y);
 			direct = 8;
-
+			checkAndFlipLine(player1,x,y,direct);
 		}
-
 		return direct;
 	}
 
-
-	public void checkAndFlipLine1(boolean player1, int x, int y) {
+	public void checkAndFlipLine(boolean player1, int x, int y, int direct) {
 
 		//指定されたマス
 		int assign = (y-1)*8+x-1;
+		
+		//方向によって変える値
+		int d = 0;
+		
+		switch(direct) {
+		case 1: d = -1;
+			break;
+		case 2: d = -9;
+			break;
+		case 3: d = -8;
+			break;
+		case 4: d = -7;
+			break;
+		case 5: d = 1;
+			break;
+		case 6: d = 9;
+			break;
+		case 7: d = 8;
+			break;
+		case 8: d = 7;
+			break;
+		}
 
 		int a;
 		int b;
@@ -323,237 +336,18 @@ public class Board {
 		}
 
 		for(int i=1; i<=6; i++) {
-			if(masu[assign-1-i]==0) {
+			if(masu[assign+d+d*i]==0) {
 				break;
 			}
-			if(masu[assign-1-i]==a) {
+			if(masu[assign+d+d*i]==a) {
 				continue;
 			}
-			if(masu[assign-1-i]==b) {
+			if(masu[assign+d+d*i]==b) {
 
 				for(int j=1; j<=i; j++) {
-					masu[assign-1-i+j]=b;
+					masu[assign+d+d*i-d*j]=b;
 				}
 			}
 		}
 	}
-
-	public void checkAndFlipLine2(boolean player1, int x, int y) {
-
-		//指定されたマス
-		int assign = (y-1)*8+x-1;
-
-		int a;
-		int b;
-		if(player1) {
-			a = 2;
-			b = 1;
-		}else {
-			a = 1;
-			b = 2;
-		}
-
-		for(int i=1; i<=6; i++) {
-			if(masu[assign-9-9*i]==0) {
-				break;
-			}
-			if(masu[assign-9-9*i]==a) {
-				continue;
-			}
-			if(masu[assign-9-9*i]==b) {
-
-				for(int j=1; j<=i; j++) {
-					masu[assign-9-9*i+9*j]=b;
-				}
-			}
-		}
-	}
-	public void checkAndFlipLine3(boolean player1, int x, int y) {
-
-		//指定されたマス
-		int assign = (y-1)*8+x-1;
-
-		int a;
-		int b;
-		if(player1) {
-			a = 2;
-			b = 1;
-		}else {
-			a = 1;
-			b = 2;
-		}
-
-		for(int i=1; i<=6; i++) {
-			if(masu[assign-8-8*i]==0) {
-				break;
-			}
-			if(masu[assign-8-8*i]==a) {
-				continue;
-			}
-			if(masu[assign-8-8*i]==b) {
-
-				for(int j=1; j<=i; j++) {
-					masu[assign-8-8*i+8*j]=b;
-				}
-			}
-		}
-	}
-
-	public void checkAndFlipLine4(boolean player1, int x, int y) {
-
-		//指定されたマス
-		int assign = (y-1)*8+x-1;
-
-		int a;
-		int b;
-		if(player1) {
-			a = 2;
-			b = 1;
-		}else {
-			a = 1;
-			b = 2;
-		}
-
-		for(int i=1; i<=6; i++) {
-			if(masu[assign-7-7*i]==0) {
-				break;
-			}
-			if(masu[assign-7-7*i]==a) {
-				continue;
-			}
-			if(masu[assign-7-7*i]==b) {
-
-				for(int j=1; j<=i; j++) {
-					masu[assign-7-7*i+7*j]=b;
-				}
-			}
-		}
-	}
-	public void checkAndFlipLine5(boolean player1, int x, int y) {
-
-		//指定されたマス
-		int assign = (y-1)*8+x-1;
-
-		int a;
-		int b;
-		if(player1) {
-			a = 2;
-			b = 1;
-		}else {
-			a = 1;
-			b = 2;
-		}
-
-		for(int i=1; i<=6; i++) {
-			if(masu[assign+1+i]==0) {
-				break;
-			}
-			if(masu[assign+1+i]==a) {
-				continue;
-			}
-			if(masu[assign+1+i]==b) {
-
-				for(int j=1; j<=i; j++) {
-					masu[assign+1+i-j]=b;
-				}
-			}
-		}
-	}
-
-	public void checkAndFlipLine6(boolean player1, int x, int y) {
-
-		//指定されたマス
-		int assign = (y-1)*8+x-1;
-
-		int a;
-		int b;
-		if(player1) {
-			a = 2;
-			b = 1;
-		}else {
-			a = 1;
-			b = 2;
-		}
-
-		for(int i=1; i<=6; i++) {
-			if(masu[assign+9+9*i]==0) {
-				break;
-			}
-			if(masu[assign+9+9*i]==a) {
-				continue;
-			}
-			if(masu[assign+9+9*i]==b) {
-
-				for(int j=1; j<=i; j++) {
-					masu[assign+9+9*i-9*j]=b;
-				}
-			}
-		}
-	}
-
-
-
-	public void checkAndFlipLine7(boolean player1, int x, int y) {
-
-		//指定されたマス
-		int assign = (y-1)*8+x-1;
-
-		int a;
-		int b;
-		if(player1) {
-			a = 2;
-			b = 1;
-		}else {
-			a = 1;
-			b = 2;
-		}
-
-		for(int i=1; i<=6; i++) {
-			if(masu[assign+8+8*i]==0) {
-				break;
-			}
-			if(masu[assign+8+8*i]==a) {
-				continue;
-			}
-			if(masu[assign+8+8*i]==b) {
-
-				for(int j=1; j<=i; j++) {
-					masu[assign+8+8*i-8*j]=b;
-				}
-				break;
-			}
-		}
-	}
-
-	public void checkAndFlipLine8(boolean player1, int x, int y) {
-
-		//指定されたマス
-		int assign = (y-1)*8+x-1;
-
-		int a;
-		int b;
-		if(player1) {
-			a = 2;
-			b = 1;
-		}else {
-			a = 1;
-			b = 2;
-		}
-
-		for(int i=1; i<=6; i++) {
-			if(masu[assign+7+7*i]==0) {
-				break;
-			}
-			if(masu[assign+7+7*i]==a) {
-				continue;
-			}
-			if(masu[assign+7+7*i]==b) {
-
-				for(int j=1; j<=i; j++) {
-					masu[assign+7+7*i-7*j]=b;
-				}
-			}
-		}
-	}
-
 }
